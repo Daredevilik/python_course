d4bccd1aeffa6212fc29138b9e7c4115d0d8f939
Dmitry <74923943+Daredevilik@users.noreply.github.com>
2020-11-25 20:35:43 +0200

    1 commit 2 commit

---
+++ lesson2.py
@@ -0,0 +1,18 @@
+import math
+#import math as mt - Дать другое название библиотеке
+#from math import sqrt - выбрать функцию из библиотеки
+
+a = 107.332
+b = math.ceil(a)
+print(b)
+
+c = math.floor(a)
+print(c)
+
+number = [1, 2, 3, 4, 5.4, 4.3]
+count = sum(number)
+print(count)
+
+z = 25
+sqr_root = math.sqrt(z)
+print(sqr_root)
