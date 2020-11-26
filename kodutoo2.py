af586dc5b8778ac069c2e81a04e2c08f6dda0675
Dmitry <74923943+Daredevilik@users.noreply.github.com>
2020-11-26 13:43:50 +0200

    Update kodutoo2.py

--- kodutoo2.py
+++ kodutoo2.py
@@ -15,6 +15,7 @@ for char in isikukood:
         aasta = int(s[1:3])
         kuu = int(s[3:5])
         paev = int(s[5:7])
+        region = int(s[7:10])
         if sugu > 6 or aasta > 99 or kuu > 12 or paev > 31:
             print("Vale isikukood!")
         elif sugu == 1 or sugu == 3 or sugu == 5:
@@ -23,3 +24,29 @@ for char in isikukood:
             print("Aitah! Te olete näine ja teie sünnipäev: ", paev, ".", kuu, ".19", aasta, sep="")
         else:
             print("Ne znaju zachem ese else nuzen, no pust budet")
+        if 0 <= region <= 10:
+            print("Te olite sündinud Kuressaare Haiglas")
+        elif 11 <= region <= 19:
+            print("Te olite sündinud Kuressaare Tartu Ülikooli Naistekliinikus, Tartumaa, Tartu")
+        elif 21 <= region <= 220:
+            print("Te olite sündinud Kuressaare  Ida-Tallinna Keskhaiglas või Pelgulinna sünnitusmajas või Hiiumaal või Keilas või Rapla haiglas või Loksa haiglas")
+        elif 221 <= region <= 270:
+            print("Te olite sündinud Ida-Viru Keskhaiglas (Kohtla-Järve, endine Jõhvi)")
+        elif 271 <= region <= 370:
+            print("Te olite sündinud Maarjamõisa Kliinikumis (Tartu) või Jõgeva Haiglas")
+        elif 371 <= region <= 420:
+            print("Te olite sündinud Narva Haiglas")
+        elif 421 <= region <= 470:
+            print("Te olite sündinud Pärnu Haiglas")
+        elif 471 <= region <= 490:
+            print("Te olite sündinud Pelgulinna Sünnitusmajas (Tallinn) või Haapsalu haiglas")
+        elif 491 <= region <= 520:
+            print("Te olite sündinud Järvamaa Haiglas (Paide)")
+        elif 521 <= region <= 570:
+            print("Te olite sündinud Rakveres või Tapa haiglas")
+        elif 571 <= region <= 600:
+            print("Te olite sündinud Valga Haiglas")
+        elif 601 <= region <= 650:
+            print("Te olite sündinud Viljandi Haiglas")
+        elif 651 <= region <= 700:
+            print("Te olite Lõuna-Eesti Haiglas (Võru) või Põlva Haiglas")
