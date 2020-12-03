639388e7d76953b07068f7d0589689754fb21b6d
Dmitry <74923943+Daredevilik@users.noreply.github.com>
2020-12-03 20:40:14 +0200

    commit

--- lesson6.py
+++ lesson6.py
@@ -4,14 +4,17 @@ def choose1():
 def choose2():
     print('Option2')
 
+def check():
+    print('Please choose option:\n 1) ..... \n 2) ......... \n 3) Close program\n')
+    choose = int(input('Type here: '))
 
-print('Please choose option:\n 1) ..... \n 2) ......... \n 3) Close program\n')
-choose = int(input('Type here: '))
-
-if choose == 1:
-    choose1()
-elif choose == 2:
-    choose2()
-elif choose == 3:
-    print('Thank you!')
-    exit()
+    if choose == 1:
+        choose1()
+    elif choose == 2:
+        choose2()
+    elif choose == 3:
+        print('Thank you!')
+        exit()
+    else:
+        print('Choose from 1 to 3')
+        check()
