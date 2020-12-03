649aa53ab524ad111ed345abf71b3f9b82c69761
Dmitry <74923943+Daredevilik@users.noreply.github.com>
2020-12-03 20:44:59 +0200

    commit

--- lesson6.py
+++ lesson6.py
@@ -6,20 +6,20 @@ def choose2():
 
 def check():
     print('Please choose option:\n 1) ..... \n 2) ......... \n 3) Close program\n')
-    choose = int(input('Type here: '))
+    choose = int(input('Type here:'))
 
     if choose == 1:
         choose1()
     elif choose == 2:
         choose2()
     elif choose == 3:
-        print('Thank you!')
+        print('\nThank you!')
         exit()
     else:
-        print('Choose from 1 to 3')
+        print('\nChoose from 1 to 3')
         check()
 try:
     check()
 except ValueError:
-    print('Only numbers!')
+    print('\nOnly numbers!')
     check()
